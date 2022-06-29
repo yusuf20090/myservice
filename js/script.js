6864287561ea74a9d5f77815e42b50e3c919d6ec
yusuf20090 <103944934+yusuf20090@users.noreply.github.com>
2022-06-29 16:29:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let Iframe = document.querySelector('iframe');
+let Wrapper = document.querySelector('.wrapper')
+uerySelector('#order .submit-wrapper');
+Iframe.onload = function () {
+alert('ok')
+}
