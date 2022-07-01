441c1afa9f2f381100e9b43b66450322010ca480
yusuf20090 <103944934+yusuf20090@users.noreply.github.com>
2022-07-01 12:24:06 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-let Iframe = document.querySelector('iframe');
-let Wrapper = document.querySelector('.wrapper')
-uerySelector('#order .submit-wrapper');
-Iframe.onload = function () {
-alert('ok')
-}
